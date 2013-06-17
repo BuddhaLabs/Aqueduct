30f19a4271cd8ce32146c9b8c7c315fc095caa1a
Leam Hall <leamhall@gmail.com>
2013-06-17 19:47:43 -0400

    #56 fixed.

--- compliance/bash/stig/rhel-6/dev/RHEL-06-000056.sh
+++ compliance/bash/stig/rhel-6/dev/RHEL-06-000056.sh
@@ -64,7 +64,17 @@
 PDI=RHEL-06-000056
 #
 #BEGIN_CHECK
+
+grep ^password /etc/pam.d/system-auth | grep dcredit > /dev/null
+
 #END_CHECK
 #BEGIN_REMEDY
+
+if [ $? -eq 1 ]
+then
+    sed -i 's/pam_cracklib.so /pam_cracklib.so dcredit=-1 /' /etc/pam.d/system-auth
+fi
+
+
 #END_REMEDY
 
